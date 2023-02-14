092d638da48aecfa17bb7b08d6bfd9799e261d8a
JacobJax <jacobjax617@gmail.com>
2023-02-15 01:04:03 +0300

    create navbar

---
+++ pitayr/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar() {
+   return (
+      <nav>
+         <div id='nav-cont' className="span-lg">
+            <h3 className="logo">🐲 Pitayr</h3>
+            <a href="#" className="btn btn-primary">Login</a>
+         </div>
+      </nav>
+   )
+}
+
+export default Navbar
