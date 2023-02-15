7a2c635bd4bd6174453467325d20ca91335ac4bf
JacobJax <jacobjax617@gmail.com>
2023-02-15 12:42:49 +0300

    create footer component

--- pitayr/src/components/Aside.jsx
+++ pitayr/src/components/Aside.jsx
@@ -1,10 +1,12 @@
 import './Aside.css'
 import Recomendations from './Recomendations'
+import Footer from './Footer'
 
 function Aside() {
    return (
       <aside>
          <Recomendations />
+         <Footer />
       </aside>
    )
 }
